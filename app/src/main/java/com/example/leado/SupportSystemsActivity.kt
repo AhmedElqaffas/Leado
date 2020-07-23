53836bb7145b609eb3bca39177c2edd299d8bfb8
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-23 09:28:13 +0200

    Added an initial design for the 'Support Systems Course' screen.

---
+++ app/src/main/java/com/example/leado/SupportSystemsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.leado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupportSystemsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_systems)
+    }
+}
