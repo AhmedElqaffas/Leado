86e58ef6d05ca5c397c217ff47001f14b7f7b4f9
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-23 10:12:05 +0200

    Added a surface view 'Support Systems Activity' which holds a sample video and used the Media Player class to play the video.

--- app/src/main/java/com/example/leado/SupportSystemsActivity.kt
+++ app/src/main/java/com/example/leado/SupportSystemsActivity.kt
@@ -1,11 +1,41 @@
 package com.example.leado
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import kotlinx.android.synthetic.main.activity_support_systems.*
 
-class SupportSystemsActivity : AppCompatActivity() {
+class SupportSystemsActivity : AppCompatActivity(), SurfaceHolder.Callback {
+    lateinit var mediaPlayer : MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support_systems)
+            var surfaceHolder = course_video_player.holder
+            surfaceHolder.setFixedSize(800, 500)
+            surfaceHolder.addCallback(this)
+            mediaPlayer = MediaPlayer.create(this, R.raw.sample_video)
+
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+        mediaPlayer.setDisplay(p0)
+        mediaPlayer.start()
+    }
+
+    override fun onPause() {
+        if(mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            mediaPlayer.release()
+        }
+        super.onPause()
+
     }
 }
