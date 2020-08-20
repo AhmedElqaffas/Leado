51ad36f0f0bd67900b46dcb298d442bbb8951a25
Ahmed Bakry <bbakry20@gmail.com>
2020-08-20 12:59:56 +0200

    Sections became clickable

--- app/src/main/java/com/example/leado/JourneyActivity.kt
+++ app/src/main/java/com/example/leado/JourneyActivity.kt
@@ -19,10 +19,11 @@ class JourneyActivity : AppCompatActivity() {
 
         setSections(getFakeSections())
 
-        populateLessonsRecyclerView(getFakeLessons())
+        getFakeLessons("Support Systems")?.let { populateLessonsRecyclerView(it) }
         textView_courseName.text = "Self-awareness"
         textView_roomName.text = "Awareness Room"
     }
+
 
     private fun setCourseProgress(progress:Int) {
         lesson1_progress.setBackgroundResource(getProgressBackground(true))
@@ -33,19 +34,27 @@ class JourneyActivity : AppCompatActivity() {
     private fun getProgressBackground(isActive : Boolean) = if(isActive) R.drawable.rounded_background_active
         else R.drawable.rounded_background_inactive
 
+
     private fun setSections(sections: List<Section>) {
         imageView_section1.setImageResource(if(sections[0].isActive) sections[0].imageId else R.drawable.info)
         textView_section1.text = sections[0].name
         textView_section1.setTextColor(if(sections[0].isActive) ResourcesCompat.getColor(resources,R.color.text_color,null) else ResourcesCompat.getColor(resources,R.color.text_color_light,null))
         textView_section1.setBackgroundResource(if(sections[0].isActive) R.drawable.rounded_background_white else R.drawable.rounded_background_white_transparent)
         done_imageView_section1.visibility = if(sections[0].isDone) View.VISIBLE else View.INVISIBLE
-
+        textView_section1.setOnClickListener {
+            getFakeLessons(sections[0].name)?.let { it1 -> populateLessonsRecyclerView(it1) }
+        }
+        textView_section1.isEnabled = sections[0].isActive
 
         imageView_section2.setImageResource(if(sections[1].isActive) sections[1].imageId else R.drawable.info)
         textView_section2.text = sections[1].name
         textView_section2.setTextColor(if(sections[1].isActive) ResourcesCompat.getColor(resources,R.color.text_color,null) else ResourcesCompat.getColor(resources,R.color.text_color_light,null))
         textView_section2.setBackgroundResource(if(sections[1].isActive) R.drawable.rounded_background_white else R.drawable.rounded_background_white_transparent)
         done_imageView_section2.visibility = if(sections[1].isDone) View.VISIBLE else View.INVISIBLE
+        textView_section2.setOnClickListener {
+            getFakeLessons(sections[1].name)?.let { it1 -> populateLessonsRecyclerView(it1) }
+        }
+        textView_section2.isEnabled = sections[1].isActive
 
 
         imageView_section3.setImageResource(if(sections[2].isActive) sections[2].imageId else R.drawable.info)
@@ -53,6 +62,10 @@ class JourneyActivity : AppCompatActivity() {
         textView_section3.setTextColor(if(sections[2].isActive) ResourcesCompat.getColor(resources,R.color.text_color,null) else ResourcesCompat.getColor(resources,R.color.text_color_light,null))
         textView_section3.setBackgroundResource(if(sections[2].isActive) R.drawable.rounded_background_white else R.drawable.rounded_background_white_transparent)
         done_imageView_section3.visibility = if(sections[2].isDone) View.VISIBLE else View.INVISIBLE
+        textView_section3.setOnClickListener {
+            getFakeLessons(sections[2].name)?.let { it1 -> populateLessonsRecyclerView(it1) }
+        }
+        textView_section3.isEnabled = sections[2].isActive
 
 
         imageView_section4.setImageResource(if(sections[3].isActive) sections[3].imageId else R.drawable.info)
@@ -60,6 +73,10 @@ class JourneyActivity : AppCompatActivity() {
         textView_section4.setTextColor(if(sections[3].isActive) ResourcesCompat.getColor(resources,R.color.text_color,null) else ResourcesCompat.getColor(resources,R.color.text_color_light,null))
         textView_section4.setBackgroundResource(if(sections[3].isActive) R.drawable.rounded_background_white else R.drawable.rounded_background_white_transparent)
         done_imageView_section4.visibility = if(sections[3].isDone) View.VISIBLE else View.INVISIBLE
+        textView_section4.setOnClickListener {
+            getFakeLessons(sections[3].name)?.let { it1 -> populateLessonsRecyclerView(it1) }
+        }
+        textView_section4.isEnabled = sections[3].isActive
 
     }
 
@@ -95,36 +112,42 @@ class JourneyActivity : AppCompatActivity() {
         ))
         return sections
     }
-    private fun getFakeLessons() : List<Lesson>{
+
+    val sectionsHash = HashMap<String,List<Lesson>>();
+    private fun getFakeLessons(sectionName : String) : List<Lesson>?{
+        if(sectionsHash.containsKey(sectionName))
+            return sectionsHash[sectionName];
         val lessons = mutableListOf<Lesson>()
         lessons.add(Lesson(
             "Lesson 1",
-            "Support System",
+            sectionName,
             "Building an integral support system,\n" +
                     "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas",
             true
         ))
         lessons.add(Lesson(
             "Lesson 2",
-            "Support System",
+            sectionName,
             "Building an integral support system,\n" +
                     "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas",
             true
         ))
         lessons.add(Lesson(
             "Lesson 3",
-            "Support System",
+            sectionName,
             "Building an integral support system,\n" +
                     "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas",
             false
         ))
         lessons.add(Lesson(
             "Lesson 4",
-            "Support System",
+            sectionName,
             "Building an integral support system,\n" +
                     "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas",
             false
         ))
+
+        sectionsHash[sectionName] = lessons;
         return lessons
     }
 }
