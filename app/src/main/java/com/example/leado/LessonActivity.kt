5970c2a64c4f633728449cf354cf3e1fddefb48d
Ahmed Bakry <bbakry20@gmail.com>
2020-08-20 16:47:38 +0200

    Youtube added instead of the local media player

--- app/src/main/java/com/example/leado/LessonActivity.kt
+++ app/src/main/java/com/example/leado/LessonActivity.kt
@@ -5,44 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.SurfaceHolder
 import androidx.core.graphics.toColorInt
-import kotlinx.android.synthetic.main.activity_journey.*
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import kotlinx.android.synthetic.main.activity_lesson.*
 
-class LessonActivity : AppCompatActivity(), SurfaceHolder.Callback {
-    lateinit var mediaPlayer : MediaPlayer
+class LessonActivity : YouTubeBaseActivity() , YouTubePlayer.OnInitializedListener{
+
+    val apiKey = "AIzaSyCsXYSFIdEiNn3kvHWYHAj4raCuFVN0ESA"
+    val id = "tGdsOXZpyWE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson)
-            var surfaceHolder = course_video_player.holder
-            surfaceHolder.setFixedSize(800, 500)
-            surfaceHolder.addCallback(this)
-            mediaPlayer = MediaPlayer.create(this, R.raw.sample_video)
 
         setCourseProgress(intent.getIntExtra("Lesson Number",1))
-
         lessonText.text = intent.getStringExtra("Section Name")
         lesson_number_text.text = intent.getStringExtra("Lesson Name")
-    }
 
-    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
-    }
+        lesson_player.initialize(apiKey,this)
 
-    override fun surfaceDestroyed(p0: SurfaceHolder?) {
-    }
-
-    override fun surfaceCreated(p0: SurfaceHolder?) {
-        mediaPlayer.setDisplay(p0)
-        mediaPlayer.start()
-    }
-
-    override fun onPause() {
-        if(mediaPlayer.isPlaying) {
-            mediaPlayer.pause()
-            mediaPlayer.release()
-        }
-        super.onPause()
 
     }
+
+
 
     private fun setCourseProgress(progress:Int) {
         progress_card_1.setCardBackgroundColor(getProgressColor(progress>=1))
@@ -53,5 +38,17 @@ class LessonActivity : AppCompatActivity(), SurfaceHolder.Callback {
     }
     private fun getProgressColor(isActive : Boolean) = if(isActive) "#00DC84".toColorInt()
         else "#9A9C9C9C".toColorInt()
+
+    override fun onInitializationSuccess( p0: YouTubePlayer.Provider?, youTubePlayer: YouTubePlayer?, wasRestored: Boolean) {
+        if(!wasRestored)
+        {
+            youTubePlayer?.cueVideo(id)
+            youTubePlayer?.play()
+        }
+    }
+
+    override fun onInitializationFailure( p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+        TODO("Not yet implemented")
+    }
 
 }
